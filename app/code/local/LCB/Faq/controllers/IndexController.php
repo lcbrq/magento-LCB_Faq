315b3ba727fba8608c269f8739c11e83efae0e9e
Silpion <tomasz@silpion.com.pl>
2015-12-13 04:00:39 +0100

    Breadcrumbs conditional fix

--- app/code/local/LCB/Faq/controllers/IndexController.php
+++ app/code/local/LCB/Faq/controllers/IndexController.php
@@ -15,16 +15,20 @@ class LCB_Faq_IndexController extends Mage_Core_Controller_Front_Action {
         $this->loadLayout();
         $this->getLayout()->getBlock("head")->setTitle($this->__("FAQ"));
         $breadcrumbs = $this->getLayout()->getBlock("breadcrumbs");
-        $breadcrumbs->addCrumb("home", array(
-            "label" => $this->__("Home Page"),
-            "title" => $this->__("Home Page"),
-            "link" => Mage::getBaseUrl()
-        ));
 
-        $breadcrumbs->addCrumb("faq", array(
-            "label" => $this->__("FAQ"),
-            "title" => $this->__("FAQ")
-        ));
+        if ($breadcrumbs) {
+
+            $breadcrumbs->addCrumb("home", array(
+                "label" => $this->__("Home Page"),
+                "title" => $this->__("Home Page"),
+                "link" => Mage::getBaseUrl()
+            ));
+
+            $breadcrumbs->addCrumb("faq", array(
+                "label" => $this->__("FAQ"),
+                "title" => $this->__("FAQ")
+            ));
+        }
 
         $this->renderLayout();
     }
