018d902bf555f7d011910520a09172ab16a8caeb
Tomasz Gregorczyk <tomasz@silpion.com.pl>
2023-02-24 15:22:34 +0100

    Version bump to 1.5.0

--- app/code/local/LCB/Faq/Model/Mysql4/Faq/Collection.php
+++ app/code/local/LCB/Faq/Model/Mysql4/Faq/Collection.php
@@ -18,7 +18,7 @@ class LCB_Faq_Model_Mysql4_Faq_Collection extends Mage_Core_Model_Mysql4_Collect
     {
         $this->addFieldToFilter('store_id', array(
             array('finset' => array('0')),
-            array('finset' => array($storeId))));
+            array('finset' => array($storeId)), ));
 
         return $this;
     }
